493b62d8e82b55fcdbc87d1e3d95290837e9a986
onifadjosh <onifadjosh@gmail.com>
2022-09-17 22:22:20 +0100

    tribute to the queen

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+
+    backgroundImage: {
+      'queen': "url('images/img4.jpg')",
+      'queen1': "url('images/img2.jpg')",
+      
+    },
+    fontFamily: {
+      'headings': ['"Space Grotesk"'],
+      'body': ['"Source Sans Pro"'],
+    },
+    extend: {
+      colors: {
+        'dark-noise': '#1F2022',
+        'primcol':'#E7E8E9',
+      },
+
+      gridTemplateColumns: {
+        
+        '3': '1fr  1fr 1fr',
+      },
+
+      gridTemplateRows: {
+        
+        '2': '1fr , 2fr',
+      }
+    },
+  },
+  plugins: [],
+}
